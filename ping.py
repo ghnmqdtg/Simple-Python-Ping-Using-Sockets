1d4bbcc7f7b2efef8474c687c582cfbe2a93e357
ghnmqdtg <ghnmqdtg@gmail.com>
2021-06-15 00:43:59 +0800

    Feat: Remake all the functions

--- ping.py
+++ ping.py
@@ -1,186 +1,210 @@
-import os
 import socket
 import struct
-import select
 import time
+import sys
+import os
 
-ICMP_ECHO_REQUEST = 8  # Seems to be the same on Solaris.
-
-ICMP_CODE = socket.getprotobyname('icmp')
-
-ERROR_DESCR = {
-    1: ' - Note that ICMP messages can only be '
-       'sent from processes running as root.',
-    10013: ' - Note that ICMP messages can only be sent by'
-           ' users or processes with administrator rights.'
-}
 
 class Ping():
     
-    def __init__(self, target_host, count=4, timeout=2):
+    def __init__(self, target_host, count=4, timeout=2, ttl=64, interval=1, ipv6=False, verbose=True, sequence=0):
         self.target_host = target_host
         self.count = count
         self.timeout = timeout
+        self.ttl = ttl
+        self.interval = interval
+        self.ipv6 = ipv6
+        self.verbose = verbose
+        self.sequence = sequence
 
     def ping(self):
-        target_ip = socket.gethostbyname(self.target_host)
-        print(f'Ping {self.target_host} [{target_ip}]')
-        for i in range(self.count):
-            try:
-                ip_header, delay = self.ping_once()
-            # Error raised for address-related errors by getaddrinfo() and getnameinfo()
-            except socket.gaierror as e:
-                print(f'Failed \nsocket error: {e[1]}')
-                break
-
-            if (delay == None):
-                print(f'Failed \nTimeout within {self.timeout} seconds')
-            else:
-                ttl = ip_header['ttl']
-                delay = round(delay * 1000.0, 4)
-                print(f'Reply from {target_ip}: bytes= time={round(delay)}ms TTL={ttl}')
-
-    # Sends one ping to the given target host
-    def ping_once(self):
+        ''' Packet header setting '''
+        self.ip_header_len = 60
+        self.icmp_header_len = 8
+        
+        if (self.ipv6):
+            self.icmp_echo_request = 128     # ICMP IPv6 ECHO_REQUEST
+            self.icmp_echo_reply = 129       # ICMP IPv6 ECHO_REPLY
+            self.addr_family = 'AF_INET6'    # IPv6 address family
+            sock_af = socket.AF_INET6
+            sock_proto = socket.getprotobyname('ipv6-icmp')
+        else:
+            self.icmp_echo_request = 8       # ICMP IPv4 ECHO_REQUEST
+            self.icmp_echo_reply = 0         # ICMP IPv4 ECHO_REPLY
+            self.addr_family = 'AF_INET'     # IPv4 address family
+            sock_af = socket.AF_INET
+            sock_proto = socket.getprotobyname('icmp')
+        
+        ''' Create the socket object '''
+        sock = None
+        
         try:
-            current_socket = socket.socket(
-                socket.AF_INET, socket.SOCK_RAW, ICMP_CODE)
-        except socket.error as e:
-            # ERROR_DESCR[1] or ERROR_DESCR[10013]: Not superuser, operation not allowed
-            if e.errno in ERROR_DESCR:
-                # Operation not permitted
-                raise socket.error(''.join((e.args[1], ERROR_DESCR[e.errno])))
-            raise  # raise the original error
+            sock = socket.socket(sock_af, socket.SOCK_RAW, sock_proto)
+        except PermissionError:
+            print('Fatal: You must be root to send ICMP packets', file=sys.stderr)
+            exit(1)
+        except:
+            print('Fatal: General error in socket()', file=sys.stderr)
+            exit(1)
+        
+        sock.settimeout(self.timeout)
+        
+        if (self.ipv6):
+            # Set IPv6 max hops
+            sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_UNICAST_HOPS, self.ttl)
+        else:
+            # IPv4 TTL
+            sock.setsockopt(socket.SOL_IP, socket.IP_TTL, self.ttl)
+
+        for seq in range(self.count):
+            ''' Prepare for the packet '''
+            icmp_code = 0
+            icmp_checksum = 0
+            # Generate 16 bit ICMP ID
+            icmp_id = os.getpid() & 0xffff
+
+            icmp_data = 192 * 'A'
+            icmp_data = bytes(icmp_data.encode('utf-8'))
+
+            data_len = len(icmp_data)
+
+            # The time that packet was created
+            timestamp_send = time.time()
+            # Generate a dummy packet with checksum = 0
+            packet = struct.pack(f'BBHHHQ{data_len}s', self.icmp_echo_request, icmp_code,
+                                icmp_checksum, icmp_id, self.sequence, int(timestamp_send), icmp_data)
+            # Get the checksum
+            icmp_checksum = self.checksum(packet)
+            # Generate the packet with generated checksum
+            packet = struct.pack(f'BBHHHQ{data_len}s', self.icmp_echo_request, icmp_code,
+                                icmp_checksum, icmp_id, self.sequence, int(timestamp_send), icmp_data)
+
+            ''' Send the packet '''
+            result = self.send_ping(
+                sock, packet, data_len, timestamp_send, icmp_id)
+            if self.count == 1:
+                # Condition ping: Host up or down
+                exit(result)
+
+            time.sleep(self.interval)
+
+        sock.close()
+
+    def send_ping(self, sock, packet, data_len, timestamp_send, icmp_id):
+        ''' Get target IP address '''
+        # target ip address
+        target_addr = None
+
+        # getaddrinfo returns an array of tuples (ainfo) for each address family and socket kind.
+        # sockaddr format: IPv4 - (target_host, port), IPv6 - (address, port, flow info, scope id)
         try:
-            host = socket.gethostbyname(self.target_host)
+            addr_info = socket.getaddrinfo(self.target_host, 1)[0]
+            # addr_info[0].name: address family, AF_INET or AF_INET6
+            # addr_info[4]: target ip address
+            print(addr_info)
+            if addr_info[0].name == self.addr_family:
+                target_addr = addr_info[4]
+        # It raises an exception if the address is unable to get
         except socket.gaierror:
-            return
-        
-        # Maximum for an unsigned short int c object counts to 65535 so
-        # we have to sure that our packet id is not greater than that.
-        packet_id = os.getpid() & 0xffff
-        
-        self.send_ping(current_socket, packet_id)
-        ip_header, delay = self.receive_ping(
-            current_socket, packet_id, self.timeout)
-        current_socket.close
+            print(
+                f'Fatal: Unable to get {self.addr_family} address for {self.target_host}', file=sys.stderr)
+            exit(1)
 
-        return ip_header, delay
+        if (self.verbose):
+            pkt=struct.unpack('BBHHHQ', packet[:-data_len])
+            host = self.target_host
+            print(f'Ping: {pkt}, {host}')
 
-    # Send ping to targe host
-    def send_ping(self, current_socket, packet_id):
-        target_addr = socket.gethostbyname(self.target_host)
-        
-        tmp_checksum = 0
+        # Send ICMP packet to the target ip address
+        try:
+            sock.sendto(packet, target_addr)
+        except socket.error:
+            etype, evalue, etrb = sys.exc_info()
+            print(evalue.args[1], file=sys.stderr)
+            return 1
 
-        # Create header with 0 checksum
-        # Header is type (8), code (8), checksum (16), id (16), sequence (16)
-        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST,
-                             0, tmp_checksum, packet_id, 1)
-        bytes_in_double = struct.calcsize('d')
-        data = (192 - bytes_in_double) * 'Q'
-        data = struct.pack('d', time.time()) + bytes(data.encode('utf-8'))
-
-        # Get the checksum on the data and header
-        tmp_checksum = self.checksum(header + data)
-        header = struct.pack('bbHHh', ICMP_ECHO_REQUEST, 0,
-                             socket.htons(tmp_checksum), packet_id, 1)
-
-        packet = header + data
-        while packet:
-            sent = current_socket.sendto(packet, (target_addr, 1))
-            packet = packet[sent:]
-        
-        return target_addr
-
-    # Reveive the ping from the socket
-    def receive_ping(self, current_socket, packet_id, timeout):
-        time_left = timeout
-        
         while True:
-            start_time = time.time()
-            ready = select.select([current_socket], [], [], time_left)
-            time_spent = time.time() - start_time
+            # calcsize('Q'): timestamp size
+            buffer_size = self.ip_header_len + self.icmp_header_len + struct.calcsize('Q') + data_len
 
-            # Timeout
-            if ready[0] == []:
-                return
-            
-            time_received = time.time()
-            recv_packet, addr = current_socket.recvfrom(1024)
-            
-            icmp_header = self.header2dict(
-                names=[
-                    'type', 'code', 'type',
-                    'packet_id', 'seq_number'
-                ],
-                struct_format='bbHHh',
-                data=recv_packet[20:28]
-            )
+            recv = None
+            host = None
 
-            if icmp_header['packet_id'] == packet_id:
-                bytes_in_double = struct.calcsize('d')
-                time_sent = struct.unpack('d', recv_packet[28:28+bytes_in_double])[0]
+            try:
+                recv, host = sock.recvfrom(buffer_size)
+            except socket.timeout:
+                print('Host is down')
+                return 1
+            except:
+                print('Fatal: General error in recvfrom()', file=sys.stderr)
+                exit(1)
+
+            timestamp_recv = time.time()
+
+            if (self.ipv6):
+                self.ip_header_len = 0
+            else:
+                # First byte consists of 4 bit IP Version and 4 bit IHL (internet header length)
+                version_ihl = struct.unpack('B', recv[:1])[0]
+                # Cut the IHL value out
+                ihl = ((version_ihl << 4) & 0xff) >> 4
+                # Recalculate the IP header length
+                self.ip_header_len = 4 * ihl
+            
+            # The length of received packet
+            packet_len = self.ip_header_len + self.icmp_header_len + \
+                struct.calcsize('Q') + data_len
+            
+            # Get packet header info
+            packet_header = struct.unpack(
+                'BBHHH', recv[self.ip_header_len:self.ip_header_len+self.icmp_header_len])
+
+            if ((packet_header[0] == self.icmp_echo_reply) and (packet_header[3] == icmp_id) and (packet_header[4] == self.sequence)):
+                if (self.verbose):
+                    pkt = packet_header[:-1]
+                    host = host[0]
+                    size = packet_len
+                    time_spent = (timestamp_recv - timestamp_send) * 1000
+                    print(f'Pong: {pkt}, {host}, {size}, {time_spent:0.2f}')
+                else:
+                    if self.count == 1:
+                        print('Host is up')
+                    else:
+                        size = packet_len
+                        host = host[0]
+                        seq = packet_header[4]
+                        time_spent = (timestamp_recv - timestamp_send) * 1000
+                        print(
+                            f'{size} bytes from {host}: seq={seq} ttl={self.ttl} time={time_spent:0.2f} ms')
                 
-                ip_header = self.header2dict(
-                    names=[
-                        'version', 'type', 'length',
-                        'id', 'flags', 'ttl', 'protocol',
-                        'checksum', 'src_ip', 'dest_ip'
-                    ],
-                    struct_format='!BBHHHBBHII',
-                    data=recv_packet[:20]
-                )
-                # Converts an IP address, which is in 32-bit packed format
-                # to the popular human readable dotted-quad string format.
-                # !: network (= big-endian), I: unsigned int
-                # ip = socket.inet_ntoa(struct.pack("!I", ip_header["src_ip"]))
-                # print(ip)
-
-                return ip_header, (time_received - time_sent)
-
-            time_left = time_left - time_spent
-
-            if time_left <= 0:
-                return
-
-    # Unpack the raw received IP and ICMP header informations to a dict
-    def header2dict(self, names, struct_format, data):
-        unpacked_data = struct.unpack(struct_format, data)
-        return dict(zip(names, unpacked_data))
-
+                return 0
+            else:
+                if timestamp_recv - timestamp_send > self.timeout:
+                    print('Host is down')
+                    return 1
+            
 
     def checksum(self, source_string):
         sum = 0
         max_count = (int(len(source_string) / 2)) * 2
-        count = 0
 
-        while count < max_count:
-            val = source_string[count+1] * 256 + source_string[count]
-            sum = sum + val
-            # Truncate sum to 32 bits
-            sum = sum & 0xffffffff
-            count = count + 2
-        
-        # Handle last byte if applicable (odd-number of bytes)
-        # Endianness should be irrelevant in this case
-        # Check for odd length
-        if max_count < len(source_string):
-            sum = sum + ord(source_string[len(source_string)-1])
-            sum = sum & 0xffffffff
+        for i in range(0, max_count, 2):
+            if i + 1 < max_count:
+                # Fold 2 neighbour bytes into a number and add it to the sum
+                sum = sum + (source_string[i+1] << 8) + source_string[i]
+            else:
+                # If there is an odd number of bytes, fake the second byte
+                sum = sum + source_string[i] + 0
 
-        # Add high 16 bits to low 16 bits
+        # # Add carry bit to the sum
         sum = (sum >> 16) + (sum & 0xffff)
-        # Add carry from above (if any)
-        sum = sum + (sum >> 16)
         # Invert and truncate to 16 bits
-        answer = ~sum
-        answer = answer & 0xffff
-        answer = answer >> 8 | (answer << 8 & 0xff00)
+        checksum = ~sum & 0xffff
 
-        return answer
+        return checksum
 
 
 if __name__ == '__main__':
-    Ping('127.0.0.1').ping()
-    Ping('www.google.com').ping()
+    # target = '127.0.0.1'
+    target = 'www.google.com'
+    Ping(target, ipv6=False, count=2, verbose=False).ping()
